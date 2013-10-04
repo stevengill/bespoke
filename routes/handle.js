31ef465326c6b2c65b93548a9cb9f14fc724046c
Steven Gill <stevengill97@gmail.com>
2013-10-04 15:58:20 -0400

    adding user data

--- routes/handle.js
+++ routes/handle.js
@@ -14,11 +14,9 @@ exports.login = function(req, res){
                 redirect_uri: 'http://bespoke.elastichance.com/handle',
                 grant_type:'authorization_code'
         }}, function(error, resp, body){
-
-            console.log(error);
-            //console.log(resp);
             console.log(body);
-            res.render('user',{title:'hi'});
+            
+            res.render('user',{image:body.user.images[138], name:body.user.display_name});
         });
     }
 }
