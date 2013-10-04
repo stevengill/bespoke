5b932837adb9a4419116d5cde99b0fa999d958fb
Isabel Barrera <ifbarrera@me.com>
2013-10-04 14:44:20 -0400

    search page

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,23 @@ function login(){
     location.href = 'https://www.behance.net/v2/oauth/authenticate?client_id=2Sfl5ZgmEkrJMIG8TRhecHnFgNO0QHTw&redirect_uri=http://bespoke.elastichance.com/&scope=collection_read|collection_write&state=boo';
 }
 
+
+
 function sendSearch(){
-    
-    
+	var params = {};
+
+ 	$.ajax({
+		url: 'https://www.behance.net/v2/projects?tags=food&api_key=2Sfl5ZgmEkrJMIG8TRhecHnFgNO0QHTw',
+		dataType: "json",
+		type: "GET",
+		cache: false,
+		success: function(data){
+			console.log(data);
+		},
+		error: function(xhr) {
+			console.log(error);
+		},
+		complete: function(){
+		}
+	}); 
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@
 /*
  * GET home page.
  */
+var request = require('request');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
+
 };
 
