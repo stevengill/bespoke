a5d5711e4162bc1cd0d15c02a13b90971d92fc0b
Steven Gill <stevengill97@gmail.com>
2013-10-04 15:28:45 -0400

    added more handle stuff

--- routes/handle.js
+++ routes/handle.js
@@ -1,4 +1,20 @@
+var request = require('request');
+var config = require('../config');
+
 exports.login = function(req, res){
-    console.log(req);
-    console.log(req.queries);
+    //console.log(req);
+    if (req.query.code){
+        console.log(req.query.code);
+        request.post('https://www.behance.net/v2/oauth/token', 
+            {
+                client_id:config.clientID, 
+                client_secret:config.clientSecret, 
+                code: req.query.code, 
+                redirect_uri: 'http://bespoke.elastichance.com/handle',
+                grant_type:'authorization_code'
+        });
+    }else{
+        console.log(req);
+        res.render('user',{title:'hi'});
+    }
 }
