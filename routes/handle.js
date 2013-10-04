fbaed704bf388ab86a419abdb2490a3cfd0a9ee8
Steven Gill <stevengill97@gmail.com>
2013-10-04 15:40:37 -0400

    attemp

--- routes/handle.js
+++ routes/handle.js
@@ -7,12 +7,14 @@ exports.login = function(req, res){
         console.log(req.query.code);
         console.log('step1');
         request.post('https://www.behance.net/v2/oauth/token', 
-            {
+            {form:{
                 client_id:config.clientID, 
                 client_secret:config.clientSecret, 
                 code: req.query.code, 
                 redirect_uri: 'http://bespoke.elastichance.com/handle',
                 grant_type:'authorization_code'
+        }}, function(error, resp, body){
+            console.log('here?');
         });
     }else{
         console.log(req);
